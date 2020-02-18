0c51f499cf428fdee10177f2ad3ec85483174e08
iepsenn <lbiepsen@inf.ufpel.edu.br>
2020-02-18 00:00:03 -0300

    criação de readme e extrator que itera com chaves da api

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-omie-extractor
+# omie-extractor
+Extrator de dados para o ERP [Omie](https://www.omie.com.br/).
+
+## Prerequisites
+Os prerequisitos são encontrados no arquivo `requirements.txt`. Para instalar basta usar:
+```
+pip3 install -r requirements.txt
+```
+
+## Arquivos de configuração
+* `keys.yaml`
+    Lista de chaves da API a serem usadas no extrator. O formato dos itens da lista é: [`app_key`, `app_secret`].
+* `routes.yaml`
+    Lista de rotas e métodos a serem acessados.
+
+## Execução
+Basta mudar os parâmetros nos arquivos de configuração e rodar o script `extractor.py`
+
+## License
+This project is licensed under the Apache License - see the LICENSE.md file for details
---
+++ extractor.py
@@ -0,0 +1,72 @@
+import requests
+import json
+import yaml
+from time import sleep
+
+url = 'https://app.omie.com.br/api/v1/'
+
+# Lendo arquivos de configuração
+with open('./keys.yaml') as file:
+    keys = yaml.load(file, Loader=yaml.FullLoader)
+
+with open('./routes.yaml') as file:
+    routes = yaml.load(file, Loader=yaml.FullLoader)
+
+# Usa todas as chaves passadas para conectar e extrair
+for app_key, app_secret in keys['keys']:
+
+    # Extrai de todas as rotas passadas
+    for route, call in routes['routes']:
+
+        print("Extracting from {} method...".format(call))
+
+        # Nome para salvar arquivos com os dados coletados
+        filename = '{}_{}.json'.format(app_key, call)
+
+        # Parâmetros padrão para requisição
+        params = {
+            "call": "{}".format(call),
+            "app_key": "{}".format(app_key),
+            "app_secret": "{}".format(app_secret),
+            "param": [
+                {
+                    "pagina": 0,
+                    "registros_por_pagina": 100,
+                    "apenas_importado_api": "N"
+                }
+            ]
+        }
+
+        data = []
+        page = 1
+
+        # Busca dados de todas as páginas
+        while True:
+
+            params['page'] = page
+
+            # Formata o url para pegar dados da página atual
+            url_api = '{}{}?JSON={}'.format(
+                url,
+                route,
+                str(params).replace('\'', "\"")
+            )
+
+            # Fazendo a request pra página
+            response = requests.get(url_api)
+
+            # Testa se deu tudo certo
+            if response.status_code == 200:
+                content = response.json()
+
+                data += list(content)
+                page += 1
+
+                sleep(3)
+            else:
+                print('Found {} pages from {} route!!'.format(page, call))
+                break
+
+        # Salva o resultado em um arquivo json
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file)
